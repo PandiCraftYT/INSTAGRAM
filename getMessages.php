fc6d8d982479658b48c3457fe785cb69331bd68a
Nevarez.Carlos <ca.nevarez20@info.uas.edu.mx>
2024-11-08 13:51:33 -0700

    Actualizacion a laravel
    implementamos todo el proyecto a laravel

--- getMessages.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $db = new PDO('pgsql:host=localhost;dbname=instagram_clone', 'user', 'password');
-
-  $stmt = $db->query("SELECT * FROM messages ORDER BY timestamp DESC");
-  $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-  echo json_encode($messages);
-?>
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
+livewire_hot_reload();
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
